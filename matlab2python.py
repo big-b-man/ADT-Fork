627ef4ec766d4a4ae1b3afec64b12f428f3fe960
big-b-man <diysandvich@gmail.com>
2025-01-23 17:23:18 -0800

    file for implementing standard matlab functions in python

---
+++ matlab2python.py
@@ -0,0 +1,21 @@
+# Aircraft design tool
+#
+# Copyright (C) 2025 Bennett Steers
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# This python file contains matlab functions that have been reimplemented in Python
+
+#atmoisa function from the aerospace toolbox
+def atmoisa(altitude):
+    return 0 #TODO: Implement function
