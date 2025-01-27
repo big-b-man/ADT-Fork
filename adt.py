0ab99f2939f84f1b9bc57741de1c51e50b349a6d
BennettUVicSubClub <diysandvich@gmail.com>
2025-01-27 15:41:25 -0800

    deleted some semicolons and other minor changes

--- adt.py
+++ adt.py
@@ -13,45 +13,50 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# This Python version is a translation of the original MATLAB file by
+# Mario Bras. Modified and translated by Bennett Steers, 2025.
+
+from fileio import read_file, load_project
 
 def adt(filename, *args):
-    print('Aircraft Design Tool  Copyright (C) 2022  Mario Bras\n');
-    print('This program comes with ABSOLUTELY NO WARRANTY.\n');
-    print('This is free software, and you are welcome to redistribute it under certain conditions;\n');
-    print('see details in the included LICENSE file.\n\n');
+    print('Aircraft Design Tool  Copyright (C) 2022  Mario Bras\n')
+    print('This program comes with ABSOLUTELY NO WARRANTY.\n')
+    print('This is free software, and you are welcome to redistribute it under certain conditions;\n')
+    print('see details in the included LICENSE file.\n\n')
 
-    # I'm probably throwing these in a separate file at some point
-    # Constants
-    class constants:
-        g = 9.81; # m/s^2
-
-    # Databases
-    class databases;
-    databases.source = read_file('energy_source_data.txt','source');
-    databases.gwp = read_file('gwp_data.txt','gwp');
+    #declaring gravity constant
+    gravity = 9.81 # m/s^2
+    
+    #importing databases 
+    source = read_file('energy_source_data.txt','source')
+    gwp = read_file('gwp_data.txt','gwp')
 
     # Load project file
-    data = load_project(filename);
+    # TODO: Implement load_project() function
+    data = load_project(filename)
 
     # Concept
-    data.concept = ahp(data.concept);
+    # This still uses matlab syntax, will have to switch to dictionary or 
+    # List syntax once i've decided what datatype will be imported by load_project
+    data.concept = ahp(data.concept)
     print_concepts(data.concept)
 
     # Add missing mission segment and vehicle component parameters
-    data.mission = build_mission(data.mission);
-    data.vehicle = build_vehicle(data.vehicle);
+    data.mission = build_mission(data.mission)
+    data.vehicle = build_vehicle(data.vehicle)
 
     # Plot mission profile
-    plot_mission(data.mission);
+    plot_mission(data.mission)
 
     # Mission analyses
-    data.vehicle = aero_analysis(data.mission, data.vehicle);
-    [data.mission, data.vehicle] = mass_analysis(data.mission, data.vehicle, data.energy);
-    data.vehicle = design_space_analysis(data.mission, data.vehicle, data.energy);
-    [data.vehicle,data.mission] = emissions(data.mission, data.vehicle);
+    data.vehicle = aero_analysis(data.mission, data.vehicle)
+    [data.mission, data.vehicle] = mass_analysis(data.mission, data.vehicle, data.energy)
+    data.vehicle = design_space_analysis(data.mission, data.vehicle, data.energy)
+    [data.vehicle,data.mission] = emissions(data.mission, data.vehicle)
 
     # Plot aircraft configuration
-    data.vehicle = plot_aircraft(data.vehicle);
+    data.vehicle = plot_aircraft(data.vehicle)
 
     # Save new project file
     if ~isempty(varargin)
